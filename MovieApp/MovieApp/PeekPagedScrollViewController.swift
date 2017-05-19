93130cede779e09ebc249d35f0df0dcf67eee57f
SeongGue <game_son20@naver.com>
2017-05-19 14:12:36 +0900

    스크롤뷰추가
    페이지 컨트롤로 스크롤뷰 추가
    아이폰 기기 별로 화면 해상도 맞춰 주는 것 해야함

---
+++ MovieApp/MovieApp/PeekPagedScrollViewController.swift
@@ -0,0 +1,133 @@
+//
+//  PeekPagedScrollViewController.swift
+//  MovieApp
+//
+//  Created by son on 2017. 5. 19..
+//  Copyright © 2017년 SonSeongGue. All rights reserved.
+//
+
+import UIKit
+
+class PeekPagedScrollViewController: UIViewController, UIScrollViewDelegate {
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var pageControl: UIPageControl!
+    
+    var pageImages: [UIImage] = []
+    var pageViews: [UIImageView?] = []
+    
+    func loadPage(_ page: Int) {
+        if page < 0 || page >= pageImages.count {
+            // If it's outside the range of what you have to display, then do nothing
+            return
+        }
+        
+        // 1
+        if let pageView = pageViews[page] {
+            // Do nothing. The view is already loaded.
+        } else {
+            // 2
+            var frame = scrollView.bounds
+            frame.origin.x = frame.size.width * CGFloat(page)
+            frame.origin.y = 0.0
+            
+            frame = frame.insetBy(dx:10.0, dy: 0.0)
+            
+            // 3
+            let newPageView = UIImageView(image: pageImages[page])
+            newPageView.contentMode = .scaleAspectFit
+            newPageView.frame = frame
+            scrollView.addSubview(newPageView)
+            
+            // 4
+            pageViews[page] = newPageView
+        }
+    }
+    
+    func purgePage(_ page: Int) {
+        if page < 0 || page >= pageImages.count {
+            // If it's outside the range of what you have to display, then do nothing
+            return
+        }
+        
+        // Remove a page from the scroll view and reset the container array
+        if let pageView = pageViews[page] {
+            pageView.removeFromSuperview()
+            pageViews[page] = nil
+        }
+    }
+    
+    func loadVisiblePages() {
+        // First, determine which page is currently visible
+        let pageWidth = scrollView.frame.size.width
+        let page = Int(floor((scrollView.contentOffset.x * 2.0 + pageWidth) / (pageWidth * 2.0)))
+        
+        // Update the page control
+        pageControl.currentPage = page
+        
+        // Work out which pages you want to load
+        let firstPage = page - 1
+        let lastPage = page + 1
+        
+        // Purge anything before the first page
+        for index in 0 ..< firstPage+1 {
+            purgePage(index)
+        }
+        
+        // Load pages in our range
+        for index in firstPage...lastPage {
+            loadPage(index)
+        }
+        
+        // Purge anything after the last page
+        for index in lastPage+1 ..< pageImages.count+1 {
+            purgePage(index)
+        }
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView!){
+        loadVisiblePages()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        pageImages = [UIImage(named: "photo1.png")!,
+                      UIImage(named: "photo2.png")!,
+                      UIImage(named: "photo3.png")!,
+                      UIImage(named: "photo4.png")!,
+                      UIImage(named: "photo5.png")!]
+        
+        let pageCount = pageImages.count
+        
+        pageControl.currentPage = 0
+        pageControl.numberOfPages = pageCount
+        
+        for _ in 0..<pageCount{
+            pageViews.append(nil)
+        }
+        
+        let pagesScrollViewSize = scrollView.frame.size
+        scrollView.contentSize = CGSize(width: pagesScrollViewSize.width * CGFloat(pageImages.count),
+                                        height: pagesScrollViewSize.height)
+        
+        loadVisiblePages()
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
